47f5f0e81f14557ed5105b93ed98bf21e883d25b
lurivero <lucasesteban.r@gmail.com>
2023-05-30 18:55:11 -0300

    Terminando funcionalidades

--- Encriptador de texto/main.js
+++ Encriptador de texto/main.js
@@ -13,7 +13,7 @@ function encriptar() {
 
   if (texto.length != 0) {
     document.getElementById("texto").value = textoCifrado;
-    tituloMensaje.textContent = "Texto Encriptado con exito";
+    tituloMensaje.textContent = "Texto encriptado con exito";
     parrafo.textContent = "";
     muneco.src = "./img/encriptado.jpg";
   } else {
@@ -22,6 +22,34 @@ function encriptar() {
     parrafo.textContent =
       "Ingresa el texto que deseas encriptar o desencriptar";
 
+    alert("Debes ingresar algun texto");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muneco = document.getElementById("muneco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con exito";
+    parrafo.textContent = "";
+    muneco.src = "./img/desencriptado.jpg";
+  } else {
+    muneco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+    parrafo.textContent =
+      "Ingresa el texto que deseas encriptar o desencriptar";
     alert("Debes ingresar algun texto");
   }
 }
